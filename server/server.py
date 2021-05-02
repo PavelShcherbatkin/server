842ee5eb2065d3ad678ac8fbe791c6534a1cac1a
PavelScherbatkin <Pahok1299@yandex.ru>
2021-05-03 01:32:59 +0300

    run_2

--- server/server.py
+++ server/server.py
@@ -1,16 +1,8 @@
-import socket
+import http.server
+import socketserver
 
-sock = socket.socket()
-sock.bind(('', 9090))
-sock.listen(1)
-conn, addr = sock.accept()
 
-print('Здесь происходить обработка перенаправленного url')
-print('connected:', addr)
-data = conn.recv(4096)
-b = data.decode('utf-8').split(' ')[1]
-url = f'http://localhost:9090' + b
-conn.send(url)
-conn.close()
-print('connection close:')
-print(url)
+handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", 9090), handler) as httpd:
+    httpd.serve_forever()
